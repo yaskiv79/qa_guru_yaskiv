b99990e51bb737a07f55b2dbbbb39440db0c3dec
Makohonchuk Valentina <yaskiv79@gmail.com>
2021-03-15 00:08:15 +0200

    test

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -19,21 +19,3 @@ public class SearchTest {
     }
 }
 
-//import org.junit.jupiter.api.Test;
-//
-//import static com.codeborne.selenide.Condition.text;
-//import static com.codeborne.selenide.Selectors.byName;
-//import static com.codeborne.selenide.Selenide.$;
-//import static com.codeborne.selenide.Selenide.open;
-//
-//public class SearchTest {
-//
-//    @Test
-//    void selenideSearchTest() {
-//        open("https://www.google.com/");
-//
-//        $(byName("q")).setValue("Selenide").pressEnter();
-//
-//        $("#search").shouldHave(text("selenide.org"));
-//    }
-//}
